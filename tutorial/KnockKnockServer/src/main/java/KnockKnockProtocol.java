e235e4e197e48ac27288a9329b8adce329c8412b
kame-git <kimitsugu.nakao@gmail.com>
2018-05-02 12:41:03 +0900

    complete KnockKnock Series

--- tutorial/KnockKnockServer/src/main/java/KnockKnockProtocol.java
+++ tutorial/KnockKnockServer/src/main/java/KnockKnockProtocol.java
@@ -31,13 +31,39 @@ public class KnockKnockProtocol {
         String theOutput = null;
 
         if (state == WAITING) {
-
+            theOutput = "Knock! Knock!";
+            state = SENTKNOCKKNOCK;
         } else if (state == SENTKNOCKKNOCK) {
-           
+            if (theInput.equalsIgnoreCase("Who's there?")) {
+                theOutput = clues[currentJoke];
+                state = SENTCLUE; 
+            } else {
+                theOutput = "You're supposed to say \"Who's there?\"! " +
+                    "Try again. Knock! Knock!";
+            }
         } else if (state == SENTCLUE) {
-
+            if (theInput.equalsIgnoreCase(clues[currentJoke] + " Who?")) {
+                theOutput = answers[currentJoke] + " Want another? (y/n)";
+                state = ANOTHER;
+            } else {
+                theOutput = "You're supposed to say \"" +
+                    clues[currentJoke] +
+                    " who?\"" + 
+                    "! Try again. Knock! Knock!";
+                state = SENTKNOCKKNOCK;
+            }
         } else if (state == ANOTHER) {
-
+            if (theInput.equalsIgnoreCase("y")) {
+                theOutput = "Knock! Knock!";
+                if (currentJoke == (NUMJOKES - 1))
+                    currentJoke = 0;
+                else
+                    currentJoke++;
+                state = SENTKNOCKKNOCK;
+            } else {
+                theOutput = "Bye.";
+                state = WAITING;
+            }
         }
         return theOutput;
     }
